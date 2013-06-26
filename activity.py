bbc3956f301553de605193f19e98ff9e0d58fac6
Ryan <ras8502@rit.edu>
2013-06-26 17:04:42 -0400

    Added the svg in the correct folder activity.info file updated to contain the correct avg started hacking on datastore manipulation in activity.py

--- activity.py
+++ activity.py
@@ -4,3 +4,34 @@ Sash Activity
  -needs to display badges in some order
  -other needs?
 """
+from sugar.activity import activity
+from sugar.datastore import datastore
+import logging
+
+class SashActivity(activity.Activity):
+
+    def __init__(self,handle):
+        print "running activity init", handle
+        activity.Activity.__init__(self, handle)
+        print "activity running"
+
+    #creates a simple textfile with filetext as the data to put into the file
+    def _writetextfile(self, filename, filetext='This is some test text'):
+        #create a datastore object
+        file_dsobject = datastore.create()
+        #write metadata
+        file_dsobject.metadata['title'] = filename
+        file_dsobject.metadata['mimetype'] = 'text/plain'
+        #write the actual file to the data directory of the activity's root
+        file_path = os.path.join(self.getactivityroot(), 'instance', filename)
+        f = open(file_path, 'w')
+        try:
+            f.write(filetext)
+        finally:
+            f.close()
+
+        #set the file_path in the datastore
+        file_dsobject.set_file_path(file_path)
+
+        datastore.write(file_dsobject)
+        return file_dsobject

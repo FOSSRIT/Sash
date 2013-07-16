1a9c9e791a5fadca2a26dfddcd7688b2286d0ae8
Ryan <ras8502@rit.edu>
2013-07-16 14:24:00 -0400

    Added DSO title search and some print out strings

--- activity.py
+++ activity.py
@@ -6,33 +6,20 @@ from sugar.datastore import datastore
 import pygame
 import logging
 
+_logger = logging.getlogger('Sash')
+
 class SashActivity(activity.Activity):
 
-    def __init__(self,handle):
+
+    def __init__(self, handle):
         print "running activity init", handle
         activity.Activity.__init__(self, handle)
         print "activity running"
 
-    #creates a simple textfile with filetext as the data to put into the file
-    def _writetextfile(self, filename, filetext='This is a test text file'):
-        #create a datastore object
-        file_dsobject = datastore.create()
-        #write metadata
-        file_dsobject.metadata['title'] = filename
-        file_dsobject.metadata['mimetype'] = 'text/plain'
-        #write the actual file to the data directory of the activity's root
-        file_path = os.path.join(self.getactivityroot(), 'instance', filename)
-        f = open(file_path, 'w')
-        try:
-            f.write(filetext)
-        finally:
-            f.close()
-
-        #set the file_path in the datastore
-        file_dsobject.set_file_path(file_path)
-
-        datastore.write(file_dsobject)
-        return file_dsobject
-
-
-
+    def findDSObjects():
+        ds_objects, num_objects = datastore.find({'title':
+        'badges_lemonadestand'})
+        print "Number of Objects: " str(num_objects)
+        for i in xrange(num_objects)
+        print "File Path" + ds_object[i].get_file_path()
+        print "Title: " + ds_object[i].metadata['title']

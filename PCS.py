3382abcb3dadf14d888427e2f12fd2a8d67b2d77
Ryan <ras8502@rit.edu>
2013-07-17 12:27:08 -0400

    borked.

--- PCS.py
+++ PCS.py
@@ -2,7 +2,7 @@
 
 from sugar.activity import activity
 from sugar.datastore import datastore
-import pygame
+#import pygame
 import logging
 
 
@@ -10,9 +10,8 @@ _logger = logging.getlogger('Sash activity')
 
 #Search journal for title of datastore object
 #and print the file path and title of DSO
-ds_objects, num_objects = datastore.find({'title': 'badges_lemonadestand'})
-print '-------------------------QUERY RESULTS---------------------'
-print "Number of Objects: " str(num_objects)
-for i in xrange(num_objects)
-print "File Path " + ds_object[i].get_file_path()
-print "Title: " + ds_object[i].metadata['title']
+ds_objects, num_objects = datastore.find({'title': 'badges_LemonadeStand'})
+print "Number of Objects: " + str(num_objects)
+for i in xrange(num_objects):
+    print "File Path " + ds_object[i].get_file_path()
+    print "Title: " + ds_object[i].metadata['title']

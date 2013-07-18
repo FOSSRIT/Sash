ae51e1513768bbf126060c429d37d82567a5d33c
Ryan <ras8502@rit.edu>
2013-07-18 16:00:26 -0400

    Updates: -added some dummy images in an images dir -activity.info updated to correct XO error -moving from pygame to gtk for Sash -PCS.py is still garbage (doesnt run yet) -activity,py is still base foundation

--- PCS.py
+++ PCS.py
@@ -1,17 +1,33 @@
 #Sash  activity
 
-from sugar.activity import activity
-from sugar.datastore import datastore
-#import pygame
-import logging
+import pygtk
+import gtk
 
+#closes application on "delete event"
+def close_application(widget, event, data = None):
+    gtk.main_quit()
+    return False
+#makes the gtk window
+def __init__(self):
+    window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    window.connect("delete_event", close_application)
+    window.show()
+#load images
+badge1 = gtk.Image()
+badge1.set_from_file("images/testpic1.png")
+#show images
+#----background
+#----badges
+badge1.show()
 
-_logger = logging.getlogger('Sash activity')
+#link to lemonade stand
 
-#Search journal for title of datastore object
-#and print the file path and title of DSO
-ds_objects, num_objects = datastore.find({'title': 'badges_LemonadeStand'})
-print "Number of Objects: " + str(num_objects)
-for i in xrange(num_objects):
-    print "File Path " + ds_object[i].get_file_path()
-    print "Title: " + ds_object[i].metadata['title']
+#badge conditionals
+
+#scroll bar
+
+#main
+def main():
+    gtk.main()
+    return 0
+
--- activity.py
+++ activity.py
@@ -1,25 +1,15 @@
-"""
-sash Activity
-"""
+#Sash Activity
+
 from sugar.activity import activity
-from sugar.datastore import datastore
-import pygame
 import logging
 
 _logger = logging.getlogger('Sash')
 
 class SashActivity(activity.Activity):
 
-
     def __init__(self, handle):
         print "running activity init", handle
         activity.Activity.__init__(self, handle)
         print "activity running"
 
-    def findDSObjects():
-        ds_objects, num_objects = datastore.find({'title':
-        'badges_lemonadestand'})
-        print "Number of Objects: " str(num_objects)
-        for i in xrange(num_objects)
-        print "File Path" + ds_object[i].get_file_path()
-        print "Title: " + ds_object[i].metadata['title']
+

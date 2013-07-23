3c12db15137ee3c525d21528897ffdee265c1e2a
Ryan <ras8502@rit.edu>
2013-07-23 11:16:05 -0400

    DOES: -window scrolls -naming conventions
    NEEDS:
    -Conditionals
    -datastore access

--- PCS.py
+++ PCS.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from gi.repository import Gtk
+import os
 
 DEFAULT_WINDOW_SIZE = {'width': 1200, 'height': 900}
 
@@ -23,6 +24,30 @@ class Sash(Gtk.Window):
                 badge = Gtk.Image(hexpand=True)
                 badge.set_from_file("badges/testpic{}.png".format(x+1))
                 self.grid.attach(badge, x, y, 1, 1)
+
+        path = os.path.join(os.path.split(__file__)[0], 'badges')
+        badges = [f for f in os.listdir(path) if os.path.isfile(
+            os.path.join(path, f))]
+
+        index = 0
+        column = 0
+        row = 0
+
+        while index < len(badges):
+            badge = Gtk.Image(hexpand=True)
+            badge.set_from_file(os.path.join(path, badges[index]))
+            badge.set_tooltip_text("Name: " + badges[index].rsplit('.', 1)[0] +
+                                   "\nDate Acquired: 7/21/13")
+            self.grid.attach(badge, row, column, 1, 1)
+
+            if row < 2:
+                row += 1
+
+            else:
+                row = 0
+                column += 1
+
+            index += 1
 
         self.scrolled_window.show_all()
 

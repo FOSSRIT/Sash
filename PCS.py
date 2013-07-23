9c51e80e0061b6e1e073e9f50f8db0f5011269ee
Ryan <ras8502@rit.edu>
2013-07-23 10:53:11 -0400

    DOES: -window scrolls correctly -renamed images folder to badges
    NEEDS:
    -conditionals
    -datastore accessability

--- PCS.py
+++ PCS.py
@@ -10,16 +10,21 @@ class Sash(Gtk.Window):
         Gtk.Window.__init__(self, title="Sash")
         self.set_default_size(DEFAULT_WINDOW_SIZE['width'],
                               DEFAULT_WINDOW_SIZE['height'])
+        self.scrolled_window = Gtk.ScrolledWindow()
+        self.scrolled_window.set_border_width(10)
+        self.scrolled_window.set_policy(
+            Gtk.PolicyType.NEVER, Gtk.PolicyType.ALWAYS)
+        self.add(self.scrolled_window)
         self.grid = Gtk.Grid(hexpand=True)
-        self.add(self.grid)
-
+        self.scrolled_window.add_with_viewport(self.grid)
+        #display badges in the grid
         for y in range(3):
             for x in range(3):
                 badge = Gtk.Image(hexpand=True)
-                badge.set_from_file("images/testpic{}.png".format(x+1))
+                badge.set_from_file("badges/testpic{}.png".format(x+1))
                 self.grid.attach(badge, x, y, 1, 1)
 
-        self.grid.show_all()
+        self.scrolled_window.show_all()
 
         self.connect("delete-event", Gtk.main_quit)
         self.show()

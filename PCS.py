da982523bb1b41ec6c8157e62e4144f9148d1622
David Wilson <daw4475@g.rit.edu>
2013-07-23 10:48:58 -0400

    Loads badges by reading all images in a directory. tooltip text

--- PCS.py
+++ PCS.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from gi.repository import Gtk
+import os
 
 DEFAULT_WINDOW_SIZE = {'width': 1200, 'height': 900}
 
@@ -10,14 +11,43 @@ class Sash(Gtk.Window):
         Gtk.Window.__init__(self, title="Sash")
         self.set_default_size(DEFAULT_WINDOW_SIZE['width'],
                               DEFAULT_WINDOW_SIZE['height'])
+
+        #self.options = Gtk.Box(spacing=10)
+        #self.add(self.options)
+
+        #self.sort_name = Gtk.Button(label="Sort By Name")
+        #self.sort_name.connect("clicked", self.sort_by_name)
+        #self.options.pack_start(self.sort_name, True, False)
+
+        #self.sort_date = Gtk.Button(label="Sort By Date")
+        #self.sort_date.connect("clicked", self.sort_by_name)
+
         self.grid = Gtk.Grid(hexpand=True)
         self.add(self.grid)
 
-        for y in range(3):
-            for x in range(3):
-                badge = Gtk.Image(hexpand=True)
-                badge.set_from_file("images/testpic{}.png".format(x+1))
-                self.grid.attach(badge, x, y, 1, 1)
+        path = os.path.join(os.path.split(__file__)[0], 'badges')
+        badges = [f for f in os.listdir(path) if os.path.isfile(
+            os.path.join(path, f))]
+
+        index = 0
+        column = 0
+        row = 0
+
+        while index < len(badges):
+            badge = Gtk.Image(hexpand=True)
+            badge.set_from_file(os.path.join(path, badges[index]))
+            badge.set_tooltip_text("Name: " + badges[index].rsplit('.', 1)[0] +
+                                   "\nDate Acquired: 7/21/13")
+            self.grid.attach(badge, row, column, 1, 1)
+
+            if row < 2:
+                row += 1
+
+            else:
+                row = 0
+                column += 1
+
+            index += 1
 
         self.grid.show_all()
 

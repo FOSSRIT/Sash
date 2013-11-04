38ca7453a5cbe3ecd807a2e88298be690e445644
David Wilson <daw4475@g.rit.edu>
2013-11-04 17:32:32 -0500

    More feedback, XO file builds even if external doesn't exist.

--- build.sh
+++ build.sh
@@ -4,19 +4,31 @@
 if [ -e /run/media/$USER/* ]; then
 
     # Build the .xo file
+    echo 'Found external device.'
+    echo 'Building xo file...'
     ./setup.py dist_xo
     file=dist/*.xo
 
     # Check if the file already exists
     if [ -e /run/media/$USER/*/$file ]; then
+        echo 'Removing version on device...'
         rm /run/media/$USER/*/$file
-    fi
+    fi 
 
     # Move the new file onto the external device
+    echo 'Moving xo file to device...'
     mv $file /run/media/$USER/*/
-    rm -rf dist/
+    echo 'Cleaning up...'
+    rm -r dist/
 
     # Notify the user a device was not found
     else
         echo 'No external device found.'
+        echo 'Building xo file...'
+        ./setup.py dist_xo
+        mv dist/* ./
+        echo 'Cleaning up...'
+        rm -r dist/
+
+    echo 'Finished!'
 fi
